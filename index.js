df2bd25ed6f75978a440d22449f1706af998afef
Omar Gonzalez <ogonzaleznyc@gmail.com>
2019-11-17 22:16:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,14 +21,17 @@ function justInvoke(fn) {
 }
 
 
+
 function setThisWithCall(fn, thisValue, arg) {
   
 }
 
 
+
 function setThisWithApply(fn, thisValue, args) {
   
 }
+
 
 
 function returnNewFunctionOf(functionToBeCopied, thisValue) {
