07b4e996e3b83ca4b3f993ad31692352e4248532
Omar Gonzalez <ogonzaleznyc@gmail.com>
2019-11-17 22:25:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
 //Your code here
 
-// function justInvoke(fn) { 
-//   return fn()
-// }
 
-// function setThisWithCall(fn, thisValue, arg) {
-//   return fn.call(thisValue, arg);
-// }
 
-// function setThisWithApply(fn, thisValue, args) {
-//   return fn.apply(thisValue, args);
-// }
 
-// function returnNewFunctionOf(functionToBeCopied, thisValue) {
-//   return functionToBeCopied.bind(thisValue);
-// }
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  return functionToBeCopied.bind(thisValue);
+}
 
 function justInvoke(fn) {
   return fn();
